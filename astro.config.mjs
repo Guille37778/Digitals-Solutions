2ccdab1b45bba33cf97901345518dd5e442d9093
Usuario <usuario@localhost>
2026-03-31 18:34:30 -0400

    otra nueva

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Guille37778.github.io',
-  base: '/Digitals-Solutions',
+  base: '/Digitals-Solutions/',
   vite: {
     plugins: [tailwindcss()]
   }
