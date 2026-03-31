aa6dd2418af239c7bdba1cd50796fd447d75f7f4
Usuario <usuario@localhost>
2026-03-31 14:07:19 -0400

    Configurar GitHub Actions y rutas de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Guille37778.github.io',
+  base: '/Digitals-Solutions',
   vite: {
     plugins: [tailwindcss()]
   }
