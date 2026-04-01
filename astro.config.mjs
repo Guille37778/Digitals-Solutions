74a7ecabb07271cf509b60a50ab08b4ad0f13884
Usuario <usuario@localhost>
2026-04-01 01:01:09 -0400

    Optimizacion Seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Guille37778.github.io',
   base: '/Digitals-Solutions/',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
